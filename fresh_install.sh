8fedd0839d6861ab461852282b3064ac0c327b50
David Da Silva <david2.dasilva.ext@orange.com>
2019-09-06 14:35:56 +0200

    script bash: install ansible PPA + run playbook

---
+++ fresh_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "======================================================"
+echo "Add Ansible PPA for latest version"
+echo "======================================================"
+sudo apt update && sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+
+echo "======================================================"
+echo "Play Ansible playbook"
+echo "======================================================"
+ansible-playbook -K main.yml
