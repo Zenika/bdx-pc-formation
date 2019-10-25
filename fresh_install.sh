2b6d2bc8c1a9a7bf5d8c369b37cfd29509bf0e64
David Da Silva <contact@daviddasilva.net>
2019-10-25 16:53:15 +0200

    split playbook into roles

--- fresh_install.sh
+++ fresh_install.sh
@@ -5,6 +5,8 @@ echo "======================================================"
 sudo apt update && sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 
+sudo apt install ansible
+
 echo "======================================================"
 echo "Play Ansible playbook"
 echo "======================================================"
